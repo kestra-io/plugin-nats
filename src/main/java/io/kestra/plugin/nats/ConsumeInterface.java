7d8312868df8c6e57e27a43e44c0eb7e7fbb9918
Loïc Mathieu <loikeseke@gmail.com>
2024-01-03 16:45:45 +0100

    feat: Micronaut 4

--- src/main/java/io/kestra/plugin/nats/ConsumeInterface.java
+++ src/main/java/io/kestra/plugin/nats/ConsumeInterface.java
@@ -4,9 +4,9 @@ import io.kestra.core.models.annotations.PluginProperty;
 import io.nats.client.api.DeliverPolicy;
 import io.swagger.v3.oas.annotations.media.Schema;
 
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import java.time.Duration;
 
 public interface ConsumeInterface {
