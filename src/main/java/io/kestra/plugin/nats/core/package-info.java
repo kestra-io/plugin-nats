f0c86feca674f7f4d71faae21d54e45c754e011a
François Delbrayelle <fdelbrayelle@gmail.com>
2026-02-13 08:26:59 +0100

    chore: update plugin categories (#107)

--- src/main/java/io/kestra/plugin/nats/core/package-info.java
+++ src/main/java/io/kestra/plugin/nats/core/package-info.java
@@ -1,6 +1,8 @@
 @PluginSubGroup(
-    description = "This subgroup of plugins contains core tasks for NATS messaging tasks and triggers.",
-    categories = PluginSubGroup.PluginCategory.MESSAGING
+    description = "This subgroup of plugins contains core tasks for NATS messaging tasks and triggers.",categories = {
+        PluginSubGroup.PluginCategory.DATA,
+        PluginSubGroup.PluginCategory.INFRASTRUCTURE
+    }
 )
 package io.kestra.plugin.nats.core;
 
--- src/main/java/io/kestra/plugin/nats/kv/package-info.java
+++ src/main/java/io/kestra/plugin/nats/kv/package-info.java
@@ -1,6 +1,8 @@
 @PluginSubGroup(
-    description = "This sub-group of plugins contains tasks for interacting with NATS key-value stores.",
-    categories = PluginSubGroup.PluginCategory.MESSAGING
+    description = "This sub-group of plugins contains tasks for interacting with NATS key-value stores.",categories = {
+        PluginSubGroup.PluginCategory.DATA,
+        PluginSubGroup.PluginCategory.INFRASTRUCTURE
+    }
 )
 package io.kestra.plugin.nats.kv;
 
